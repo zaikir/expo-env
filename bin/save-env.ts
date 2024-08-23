311a84898dcb4e4be9946e2e9a1aafbb35b5d4fe
Kirill Zaidulin <zaidulinkirill@gmail.com>
2024-08-23 10:38:16 +0300

    fix

--- bin/save-env.ts
+++ bin/save-env.ts
@@ -1,11 +1,7 @@
 #!/usr/bin/env bun
 
 import { loadEnv } from "@kirz/expo-env/src/load-env";
-import { loadEas } from "@kirz/expo-env/src/load-eas";
 import { compileTypes } from "@kirz/expo-env/src/compile-types";
 
-const { parseTypes } = await loadEnv()
-
-if (parseTypes) {
-  compileTypes(`node_modules/@kirz/expo-env/tsconfig.json`)
-}
+await loadEnv()
+compileTypes(`node_modules/@kirz/expo-env/tsconfig.json`)
